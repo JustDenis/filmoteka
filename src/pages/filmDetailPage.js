764a7e6267601182017ddee707aa61adcdfcd427
JustDenis <denisdenis2343@gmail.com>
2020-09-09 00:33:46 +0300

    commit

--- src/pages/filmDetailPage.js
+++ src/pages/filmDetailPage.js
@@ -1,7 +1,6 @@
 // import FilmPageTemplate from '../templates/film.hbs';
 import { ROOT_DOM } from '../constants';
 
-const FilmPage = () => {
 const FilmPage = (id) => {
   // const markup = FilmPageTemplate({ id });
 
