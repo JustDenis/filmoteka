2d175d148f1f48516691dcb7b4b6e4a0f75613c0
VVMachine <vik.atamanenko@gmail.com>
2020-09-08 23:17:29 +0300

    1st commit mainpage

--- src/pages/filmDetailPage.js
+++ src/pages/filmDetailPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import FilmPageTemplate from '../templates/film.hbs';
 import { ROOT_DOM } from '../constants';
 
@@ -8,13 +7,6 @@ const FilmPage = (id) => {
   // //
 
   // ROOT_DOM.innerHTML = markup;
-=======
-import FilmPageTemplate from '../templates/Film.hbs';
-import { ROOT_DOM } from '../constants';
-
-const FilmPage = () => {
-
->>>>>>> 4f5fc7a028783e33c461925064326d821870d3ee
 }
 
 export default FilmPage;
