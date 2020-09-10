b3c8ecc4d79ef3a884e3395e98e66d7e08f07af2
unknown <you@example.com>
2020-09-10 20:33:51 +0300

    addlibrary-btn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+import './styles/library.css';
 import './styles/mainPage.css';
 import './styles/FilmPage.css';
 import './styles/desktopHeaderFooter.css';
@@ -7,6 +8,7 @@ import './styles/tabletHeaderFooter.css';
 import Router from './utilites/router';
 import MainPage from './pages/mainPage';
 import FilmPage from './pages/filmDetailPage';
+import libraryPage from './pages/libraryPage.js';
 import './utilites/scrollToTop';
 import './utilites/navigation';
 
@@ -21,9 +23,15 @@ window['router'] = new Router({
       },
     },
     {
-      path: '/library/watched',
+      path: 'library/watched',
       callback: () => {
-      
+        libraryPage();
+      },
+    },
+    {
+      path: 'library/queue',
+      callback: () => {
+        libraryPage();
       },
     },
     {
