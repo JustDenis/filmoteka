c89d924b66c9c9e31163a342af8806ba90812b10
Oleg <kolyushko.oleg@gmail.com>
2020-09-10 18:19:30 +0300

    updateHeaderFooter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import './styles.css';
 import './styles/mainPage.css';
 import './styles/FilmPage.css';
 import './styles/desktopHeaderFooter.css';
-import './styles/tabletHeaderFooter.css';
 import './styles/mobileHeaderFooter.css';
+import './styles/tabletHeaderFooter.css';
 import Router from './utilites/router';
 import MainPage from './pages/mainPage';
 import FilmPage from './pages/filmDetailPage';
+import './utilites/scrollToTop';
+import './utilites/navigation';
+
 
 window['router'] = new Router({
   root: '/',
@@ -15,6 +18,12 @@ window['router'] = new Router({
       path: /film\/(.*)/,
       callback: () => {
         FilmPage();
+      },
+    },
+    {
+      path: '/library/watched',
+      callback: () => {
+      
       },
     },
     {
