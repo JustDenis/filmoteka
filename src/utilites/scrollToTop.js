6517bf2198bde7409890fab6f07a64540838db9b
Oleg <kolyushko.oleg@gmail.com>
2020-09-11 17:40:56 +0300

    updateHeaderStyles

--- src/utilites/scrollToTop.js
+++ src/utilites/scrollToTop.js
@@ -2,3 +2,14 @@ export default window.onscroll = function buttonUp() {
   document.querySelector('.btnup').style.display =
     window.pageYOffset > '200' ? 'block' : 'none';
 };
+
+const buttonUp = document.querySelector('.btnup');
+buttonUp.addEventListener('click', scrolling);
+
+function scrolling(e){
+    window.scrollTo(
+      {
+        top: 0,
+        behavior: 'smooth',
+      });
+   };
